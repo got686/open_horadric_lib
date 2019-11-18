89ba3cbe997c9aaed86ed32ef44b7f39d439aa43
got686 <got686@yandex.ru>
2019-11-18 19:08:02 +0300

    error processor overriding support

--- open_horadric_lib/proxy/proxy.py
+++ open_horadric_lib/proxy/proxy.py
@@ -13,14 +13,19 @@ from open_horadric_lib.proxy.middleware.request_id import RequestIdProxyMiddlewa
 
 class BaseProxy:
     logger = logging.getLogger("open_horadric.proxy.BaseProxy")
-    error_processor: ErrorProcessor = ErrorProcessor
 
-    def __init__(self, client: BaseClient, middlewares: List[BaseProxyMiddleware] = None):
+    def __init__(
+        self, client: BaseClient, middlewares: List[BaseProxyMiddleware] = None, error_processor: ErrorProcessor = None
+    ):
         if middlewares is None:
             middlewares = [RequestIdProxyMiddleware()]
 
+        if error_processor is None:
+            error_processor = ErrorProcessor()
+
         self.client = client
         self.middlewares = middlewares
+        self.error_processor = error_processor
 
     def bind(self, app: flask.Flask):
         raise NotImplementedError
